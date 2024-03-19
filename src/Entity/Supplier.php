cd1ac6787b270f7bb5d882ecff07944c83e9effc
mohamed <mohamed.bouaoud@pm.me>
2024-03-19 12:44:28 +0100

    + added Unique db constraint to supplier.name

--- src/Entity/Supplier.php
+++ src/Entity/Supplier.php
@@ -15,7 +15,7 @@ class Supplier
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $name = null;
 
     #[ORM\OneToMany(targetEntity: Product::class, mappedBy: 'supplier', orphanRemoval: true)]
